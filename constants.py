76315122a5f3ec092a24cbae15996a3cf1033458
jordyn-raguckas <147428738+jordyn-raguckas@users.noreply.github.com>
2025-12-28 14:40:20 -0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
-leagueID = "1609009"
-leagueStartYear = 2013
-leagueEndYear = 2024
+leagueID = "538422"
+leagueStartYear = 2011
+leagueEndYear = 2023
 
 standings_directory = './output/'+ leagueID + '-history-standings/'
 gamecenter_directory = './output/'+ leagueID + '-history-teamgamecenter/'
